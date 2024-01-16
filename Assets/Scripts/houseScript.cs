f4ff4a4d137f995f56b965f61b2c75a8f3e3808a
Vedika Patil <70337066+Vedikahello@users.noreply.github.com>
2024-01-16 10:53:39 +0530

    files added

---
+++ Assets/Scripts/houseScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class houseScript : MonoBehaviour {
+
+    public GameObject theRoof;
+    public GameObject[] secondFloor;
+    public GameObject[] theLights;
+    public GameObject directionalLight;
+
+    public static houseScript instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/vbButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class vbButton : MonoBehaviour, IVirtualButtonEventHandler{
+
+    public GameObject[] myButtons;
+    public GameObject confirmPanel;
+    public GameObject videoPlane;
+    private bool isVideoPlaying = false;
+    private VideoPlayer video;
+   
+    // Use this for initialization
+    void Start () {
+		for(int i=0; i < myButtons.Length ; i++)
+        {
+            myButtons[i].GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        }
+        video = videoPlane.GetComponent<VideoPlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if(vb.name == "buttonT3")
+        {
+            confirmPanel.SetActive(true);
+        }
+        if (vb.name == "buttonT1Start" && isVideoPlaying == false)
+        {
+            video.Play();
+            isVideoPlaying = true;
+        }
+        if (vb.name == "buttonT1Sop" && isVideoPlaying == true)
+        {
+            video.Stop();
+            isVideoPlaying = false;
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+    }
+}
